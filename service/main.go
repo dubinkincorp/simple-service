3367684b7775528294db0c157a1fa70843f8ef2d
dubina_si <dubina_si@interrao.ru>
2021-07-19 18:56:22 +0300

    init

---
+++ service/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Start server")
+
+	http.HandleFunc("/health/", handler)
+	http.ListenAndServe(":8000", nil)
+}
+
+type Status struct {
+	Status string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	status := Status{"OK"}
+
+	js, err := json.Marshal(status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
